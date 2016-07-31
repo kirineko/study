8479b366811e876d5dc2e0ede8d61495d7f9bb49
kirineko <kirinekoneko@gmail.com>
2016-07-31 21:37:40 +0800

    完成作业

--- app/task/GetHtmlTask.php
+++ app/task/GetHtmlTask.php
@@ -1,55 +1,150 @@
 <?php
 /**
- * Author: liyi
- * Datetime: 16/7/28, 上午9:56
+ * 抓取live站点类目信息
+ * @author liyi
+ * @version 1.0.0
  */
 
-$url = 'http://www.genshuixue.com/livezone';
+use Myf\Mvc\Task;
 
-$web_raw_content = file_get_contents($url);
+class GetHtmlTask extends Task {
 
-//抓取<tr>标签的所有内容
-$preg_tr = "/<tr>.*<\/tr>/";
-preg_match($preg_tr, $web_raw_content, $arr_tr);
-$table_content = $arr_tr[0];
+    /**
+     * 主方法: 抓取live页面的条目内容,并以特定格式打印输出到文件
+     * 实现: 1. 抓取网页条目内容; 2. 以特定方式写入文件
+     *
+     * 方法调用:
+     *    function _getWebContent($url): Array, 抓取网页内容并以数组形式返回
+     *    function _writeToFile($arr, $file), 将数组内容写入到文件
+     */
+    public function mainAction() {
+        //待抓取网站
+        $url = 'http://www.genshuixue.com/livezone';
 
-$preg_td = "/<td>.*?<\/td>/";
-preg_match_all($preg_td, $table_content, $arr_td, PREG_PATTERN_ORDER);
+        //抓取网页条目
+        $arr = $this->_getWebContent($url);
 
-
-foreach($arr_td[0] as $str_td_item) {
-    $preg_alink = "/<a.*?<\/a>/";
-    preg_match_all($preg_alink, $str_td_item, $arr_alink, PREG_PATTERN_ORDER);
-    print_r($arr_alink[0]);
-}
-
-die;
-$arr_res = json_decode($res, true);
-
-
-$tmp = $arr_res['data']['category'];
-
-$res = [];
-
-foreach ($tmp as $key => $value) {
-    $sub_res = [
-        'title' => [
-            'url' => $value['url'],
-            'text' => $key,
-        ],
-    ];
-    foreach ($value['sub_class'] as $sub_item) {
-        $sub = [
-            'url' => $sub_item['url'],
-            'text' => $sub_item['name'],
-        ];
-        $sub_res['list'][]= $sub;
+        //写入文件
+        $file = 'result.php';
+        $res = $this->_writeToFile($arr, $file);
+        if ($res) {
+            echo 'SUCC! 数组已经写入根目录下的: result.php 文件。';
+        } else {
+            echo 'FAIL! 写入异常!';
+        }
     }
-    $res[] = $sub_res;
+
+    /**
+     * 使用正则表达式匹配抓取网页中特定内容
+     * 1. preg_match : 定位到<tr>标签,排除<tr>标签以外的内容
+     * 2. preg_match_all: 以懒惰模式获取所有<td>标签内容并存入数组
+     * 3. preg_match_all: 以懒惰模式获取所有<a>标签内容并存入数组
+     * 4. a标签的链接地址以及a标签的元素内容就是要抓取的最终内容
+     * 5. 对抓取的内容组织成数组并返回
+     *
+     * @param $url 抓取页面的url
+     *
+     * @return $result 规定格式的数组
+     */
+    private function _getWebContent($url) {
+
+        $strContent = file_get_contents($url);
+
+        //1. 获取<tr>标签内的所有内容
+        preg_match("/<tr>.*<\/tr>/", $strContent, $arrTrContent);
+        $trContent = $arrTrContent[0];
+
+        //2. 获取<td>标签内的所有内容,正则表达式中?代表懒惰模式
+        preg_match_all("/<td>.*?<\/td>/", $trContent, $arrTdContent, PREG_PATTERN_ORDER);
+
+        //3. 循环获取所有<a>标签并写入数组
+        foreach ($arrTdContent[0] as $strItem) {
+            preg_match_all("/<a(.*?)href=\"(.*?)\"(.*?)>(.*?)<\/a>/i", $strItem, $arrAlink, PREG_PATTERN_ORDER);
+            $arrHref [] = $arrAlink[2];
+            $arrTitle [] = $arrAlink[4];
+        }
+
+        //4. 按规定样式重新组织数组
+        $result = $this->_formatArray($arrHref, $arrTitle);
+
+        return $result;
+    }
+
+    /**
+     * 按固定格式组织数组
+     *
+     * @param $arrHref 全部的URL数组
+     * @param $arrTitle 全部的Title数组
+     * @return array $result 规定格式的数组
+     */
+    private function _formatArray($arrHref, $arrTitle) {
+        //将title数组中首元素取出,作为栏目标题
+        foreach ($arrTitle as &$title) {
+            $text [] = $title[0];
+            unset($title[0]);
+        }
+
+        //将href数组中首元素取出,作为栏目url
+        foreach ($arrHref as &$href) {
+            $url [] = $href[0];
+            unset($href[0]);
+        }
+
+        //重新组织title项
+        $title = array_combine($text, $url);
+        foreach ($title as $text => $url) {
+            $resTitle [] = [
+                'url' => $url,
+                'text' => $text,
+            ];
+        }
+
+        //对arrTitle数组和arrHref数组剩余元素重新数组,构成list项目
+        foreach ($arrTitle as $key => $subArrTitle) {
+            $textList = $subArrTitle;
+            $hrefList = $arrHref[$key];
+            $lists = array_combine($textList, $hrefList);
+            $subList = [];
+            foreach ($lists as $text => $url) {
+                $subList [] = [
+                    'url' => $url,
+                    'text' => $text,
+                ];
+            }
+            $resList [] = $subList;
+        }
+
+        //按最终格式返回result数组
+        $result = [];
+        foreach ($resTitle as $key => $title) {
+            $result [] = [
+                'title' => $title,
+                'list' => $resList[$key],
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $arr 待写入的数组
+     * @param $file 待写入的文件路径
+     *
+     * @return boolean true|false
+     */
+    private function _writeToFile($arr, $file) {
+
+        $strResult = var_export($arr, true) . ';';
+
+        try {
+            $res = file_put_contents($file, "<?php \nreturn ");
+            if (!$res) {
+                return false;
+            }
+            return file_put_contents($file, $strResult, FILE_APPEND|LOCK_EX);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
 }
-
-$str_res = var_export($res, true) . ';';
-
-echo file_put_contents('result.php', "<?php \nreturn ");
-
-file_put_contents('result.php', $str_res, FILE_APPEND|LOCK_EX);
---
+++ result.php
@@ -0,0 +1,193 @@
+<?php 
+return array (
+  0 => 
+  array (
+    'title' => 
+    array (
+      'url' => 'http://www.genshuixue.com/sc/-3-107.html?source=live',
+      'text' => '中小学',
+    ),
+    'list' => 
+    array (
+      0 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-107.html?source=live',
+        'text' => '小学',
+      ),
+      1 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-140.html?source=live',
+        'text' => '小升初',
+      ),
+      2 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-161.html?source=live',
+        'text' => '初中',
+      ),
+      3 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-237.html?source=live',
+        'text' => '中考',
+      ),
+      4 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-266.html?source=live',
+        'text' => '高中',
+      ),
+      5 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-342.html?source=live',
+        'text' => '高考',
+      ),
+    ),
+  ),
+  1 => 
+  array (
+    'title' => 
+    array (
+      'url' => 'http://www.genshuixue.com/sc/-3-783.html?source=live',
+      'text' => '出国留学',
+    ),
+    'list' => 
+    array (
+      0 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-783_784.html?source=live',
+        'text' => '留学考试',
+      ),
+      1 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-783_784_785.html?source=live',
+        'text' => '托福',
+      ),
+      2 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-783_784_787.html?source=live',
+        'text' => '雅思',
+      ),
+      3 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-783_784_788.html?source=live',
+        'text' => 'SAT',
+      ),
+      4 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-783_784_793.html?source=live',
+        'text' => 'GRE',
+      ),
+    ),
+  ),
+  2 => 
+  array (
+    'title' => 
+    array (
+      'url' => 'http://www.genshuixue.com/sc/-3-975.html?source=live',
+      'text' => '艺体兴趣',
+    ),
+    'list' => 
+    array (
+      0 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-897_917.html?source=live',
+        'text' => '时尚',
+      ),
+      1 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-897.html?source=live',
+        'text' => '生活',
+      ),
+      2 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-878.html?source=live',
+        'text' => '兴趣',
+      ),
+      3 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-897_1196_1198.html?source=live',
+        'text' => '情感',
+      ),
+      4 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-975.html?source=live',
+        'text' => '艺术',
+      ),
+      5 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-921.html?source=live',
+        'text' => '体育',
+      ),
+    ),
+  ),
+  3 => 
+  array (
+    'title' => 
+    array (
+      'url' => 'http://www.genshuixue.com/sc/-3-714.html?source=live',
+      'text' => '语言培训',
+    ),
+    'list' => 
+    array (
+      0 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-387_410_411.html?source=live',
+        'text' => '考研英语',
+      ),
+      1 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-387_388.html?source=live',
+        'text' => '英语考级',
+      ),
+      2 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-714_743_744.html?source=live',
+        'text' => '日语',
+      ),
+      3 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-714_743_751.html?source=live',
+        'text' => '法语',
+      ),
+      4 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-714_715_729.html?source=live',
+        'text' => '英语口语',
+      ),
+    ),
+  ),
+  4 => 
+  array (
+    'title' => 
+    array (
+      'url' => 'http://www.genshuixue.com/sc/-3-502.html?source=live',
+      'text' => '技能&考试',
+    ),
+    'list' => 
+    array (
+      0 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-502_1134.html?source=live',
+        'text' => 'http://www.genshuixue.com/sc/-3-107.html?source=live',
+      ),
+      1 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-502_542.html?source=live',
+        'text' => 'http://www.genshuixue.com/sc/-3-783.html?source=live',
+      ),
+      2 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-897_915.html?source=live',
+        'text' => 'http://www.genshuixue.com/sc/-3-975.html?source=live',
+      ),
+      3 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-817.html?source=live',
+        'text' => 'http://www.genshuixue.com/sc/-3-714.html?source=live',
+      ),
+      4 => 
+      array (
+        'url' => 'http://www.genshuixue.com/sc/-3-387_410.html?source=live',
+        'text' => 'http://www.genshuixue.com/sc/-3-502.html?source=live',
+      ),
+    ),
+  ),
+);
